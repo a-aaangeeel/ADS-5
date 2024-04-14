84099f9b6a37ad4867cd4504d8f56f67b3fe58dd
Nana <160034837+a-aaangeeel@users.noreply.github.com>
2024-04-14 16:33:00 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,21 +1,21 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int max_size>
 class TStack {
-private:
+ private:
     const int Ksize = max_size;
     T array[100];
     int top_index;
 
-public:
+ public:
     TStack() : top_index(0) {}
     T get() {
         if (!isEmpty()) {
             return array[top_index - 1];
-        }
-        else {
+        } else {
             throw std::string("Empty");
         }
     }
@@ -28,19 +28,16 @@ public:
     T pop() {
         if (top_index > 0) {
             return array[--top_index];
-        }
-        else {
+        } else {
             throw std::string("Empty");
         }
     }
     void push(const T& item) {
         if (top_index <= max_size - 1) {
             array[top_index++] = item;
-        }
-        else {
+        } else {
             throw std::string("Full");
         }
     }
 };
-
 #endif  // INCLUDE_TSTACK_H_
