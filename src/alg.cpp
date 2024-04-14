d479123831a8594da275a6962a29ce39ecefdf36
Nana <160034837+a-aaangeeel@users.noreply.github.com>
2024-04-14 15:50:02 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,87 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int precedence(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(std::string inf) {
+    TStack <char, 100> stack;
+    std::stringstream postfix;
+
+    for (char c : inf) {
+        if (c == ' ')
+            continue;
+        if (isdigit(c)) {
+            postfix << c;
+            continue;
+        }
+        if (c == '(') {
+            stack.push(c);
+            continue;
+        }
+        if (c == ')') {
+            while (!stack.isEmpty() && stack.get() != '(') {
+                postfix << ' ' << stack.get();
+                stack.pop();
+            }
+            stack.pop();
+            continue;
+        }
+        if (c == '*' || c == '/' || c == '+' || c == '-') {
+            postfix << ' ';
+            while (!stack.isEmpty() && precedence(stack.get()) >= precedence(c)) {
+                postfix << stack.get() << ' ';
+                stack.pop();
+            }
+            stack.push(c);
+            continue;
+        }
+    }
+
+    while (!stack.isEmpty()) {
+        postfix << ' ' << stack.get();
+        stack.pop();
+    }
+
+    return postfix.str();
+}
+
+int eval(std::string post) {
+    TStack <int, 100> stack;
+    std::stringstream expression(post);
+    std::string token;
+    int operand1, operand2;
+
+    while (expression >> token) {
+        if (isdigit(token[0])) {
+            stack.push(std::stoi(token));
+        }
+        else if (token[0] == '*' || token[0] == '/' || token[0] == '+' || token[0] == '-') {
+            operand2 = stack.get();
+            stack.pop();
+            operand1 = stack.get();
+            stack.pop();
+            switch (token[0]) {
+            case '+':
+                stack.push(operand1 + operand2);
+                break;
+            case '-':
+                stack.push(operand1 - operand2);
+                break;
+            case '*':
+                stack.push(operand1 * operand2);
+                break;
+            case '/':
+                stack.push(operand1 / operand2);
+                break;
+            }
+        }
+    }
+
+    return stack.get();
 }
